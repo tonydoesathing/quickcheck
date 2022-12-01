759a04a58b83b8ebc38dd14f92f22655649ff8d1
Mac <mac.w.obrien@gmail.com>
2022-12-01 11:46:01 -0700

    Added copyWith to student and made id nullable

--- lib/data/model/student.dart
+++ lib/data/model/student.dart
@@ -1,10 +1,20 @@
 import 'package:equatable/equatable.dart';
 
 class Student extends Equatable {
-  final int id;
+  final int? id;
   final String name;
 
   const Student({required this.id, required this.name});
+
+  Student copyWith({
+    int? id,
+    String? name,
+  }) {
+    return Student(
+      id: id ?? this.id,
+      name: name ?? this.name,
+    );
+  }
 
   @override
   List<Object?> get props => [id, name];

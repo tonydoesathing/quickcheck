bc1e799c1a47104a623d9206d47127b51eba9f79
Tony <tonygovernmentemail@gmail.com>
2022-12-18 23:31:43 -0700

    I think I covered all the edge cases

--- lib/widgets/group_assessment_table.dart
+++ lib/widgets/group_assessment_table.dart
@@ -3,6 +3,7 @@ import 'package:quickcheck/data/model/assessment.dart';
 import 'package:quickcheck/data/model/student.dart';
 import 'package:quickcheck/data/model/group.dart';
 import 'package:quickcheck/widgets/assessment_score.dart';
+import 'package:table_sticky_headers/table_sticky_headers.dart';
 
 /// A table that displays students on the y-axis and assignments on the top column
 /// Student names are the first column, and each column after that is their score for the associated assessment,
@@ -11,104 +12,251 @@ class GroupAssessmentTable extends StatelessWidget {
   final List<Assessment> assessments;
   final List<Group> groups;
   final List<Student> students;
+  List<Student> ungroupedStudents = [];
+  List groupsAndStudents = [];
 
   /// Create a table from a list of [students] and a list of [assessments]
-  const GroupAssessmentTable(
+  GroupAssessmentTable(
       {Key? key,
       required this.assessments,
       required this.groups,
       required this.students})
-      : super(key: key);
+      : super(key: key) {
+    // generate list to render
+    for (Group group in groups) {
+      groupsAndStudents.add(group);
+      for (Student student in group.members) {
+        groupsAndStudents.add(student);
+      }
+    }
+    // get ungrouped students
+    for (Student student in students) {
+      if (student.groups == null || student.groups!.isEmpty) {
+        ungroupedStudents.add(student);
+      }
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 70.0),
-      child: DataTable(
-        columns: <DataColumn>[
-          const DataColumn(
-            label: Expanded(
+      padding: const EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 0),
+      child: StickyHeadersTable(
+        columnsLength: assessments.length,
+        rowsLength: ungroupedStudents.isNotEmpty
+            ? groupsAndStudents.length +
+                (groupsAndStudents.isNotEmpty ? 1 : 0) +
+                1 +
+                ungroupedStudents.length
+            // groups and students + blank row (if needed) + ungrouped title + ungrouped students
+            : groupsAndStudents.length,
+        cellDimensions: const CellDimensions.fixed(
+          contentCellWidth: 140.0,
+          contentCellHeight: 50.0,
+          stickyLegendWidth: 140.0,
+          stickyLegendHeight: 50.0,
+        ),
+        columnsTitleBuilder: (columnIndex) {
+          return TableCell.stickyColumn(
+              widget: Expanded(
+            child: Center(
               child: Text(
-                '',
+                assessments[columnIndex].name,
+                style: Theme.of(context).textTheme.titleMedium,
+                overflow: TextOverflow.fade,
+                textAlign: TextAlign.center,
               ),
             ),
-          ),
-          for (Assessment assessment in assessments)
-            DataColumn(
-              label: Expanded(
+          ));
+        },
+        rowsTitleBuilder: (rowIndex) {
+          // if there aren't ungrouped
+          if (rowIndex < groupsAndStudents.length) {
+            var element = groupsAndStudents[rowIndex];
+            if (element is Group) {
+              return TableCell.stickyRow(
+                  widget: Expanded(
                 child: Text(
-                  assessment.name,
-                  style: Theme.of(context).textTheme.titleMedium,
+                  element.name,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontWeight: FontWeight.bold),
+                  overflow: TextOverflow.fade,
                 ),
-              ),
-            ),
-        ],
-        rows: <DataRow>[
-          for (Group group in groups) ...[
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text(group.name,
+              ));
+            } else if (element is Student) {
+              return TableCell.stickyRow(
+                  widget: Expanded(
+                child: Row(children: [
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                      child: Text(element.name, overflow: TextOverflow.fade))
+                ]),
+              ));
+            }
+          }
+
+          // return the empty row
+          if (rowIndex == groupsAndStudents.length &&
+              groupsAndStudents.isNotEmpty) {
+            return const TableCell.stickyRow(
+              colorHorizontalBorder: Colors.transparent,
+              colorVerticalBorder: Colors.transparent,
+            );
+          }
+          // return the Ungrouped title
+          if (rowIndex ==
+              groupsAndStudents.length +
+                  (groupsAndStudents.isNotEmpty ? 1 : 0)) {
+            return TableCell.stickyRow(
+                widget: Text("Ungrouped",
                     style: Theme.of(context)
                         .textTheme
                         .titleMedium!
-                        .copyWith(fontWeight: FontWeight.bold))),
-                for (Assessment assessment in assessments)
-                  DataCell(Center(
-                      child: AssessmentScore(
-                          score: assessment.scoreMap[group] ?? -1))),
-              ],
-            ),
-            for (Student student in group.members)
-              DataRow(
-                cells: <DataCell>[
-                  DataCell(Row(children: [
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Text(student.name)
-                  ])),
-                  for (Assessment assessment in assessments)
-                    DataCell(Center(
-                      child: AssessmentScore(
-                          score: assessment.scoreMap[student] ?? -1),
-                    )),
-                ],
+                        .copyWith(fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.fade));
+          }
+// return ungrouped students
+          return TableCell.stickyRow(
+              widget: Expanded(
+            child: Row(children: [
+              const SizedBox(
+                width: 20,
               ),
-          ],
-          DataRow(
-            cells: <DataCell>[
-              DataCell(Row(children: const [
-                SizedBox(
-                  width: 20,
+              Expanded(
+                child: Text(
+                    ungroupedStudents[rowIndex -
+                            groupsAndStudents.length -
+                            1 -
+                            (groupsAndStudents.isNotEmpty ? 1 : 0)]
+                        .name,
+                    overflow: TextOverflow.fade),
+              )
+            ]),
+          ));
+        },
+        contentCellBuilder: (columnIndex, rowIndex) {
+          //the grouped
+          if (rowIndex < groupsAndStudents.length) {
+            return TableCell.content(
+              widget: Expanded(
+                child: Center(
+                  child: AssessmentScore(
+                      score: assessments[columnIndex]
+                              .scoreMap[groupsAndStudents[rowIndex]] ??
+                          -1),
                 ),
-                Text('')
-              ])),
-              for (Assessment assessment in assessments)
-                DataCell(Center(
-                  child: Container(),
-                )),
-            ],
+              ),
+            );
+          }
+          // the empty row
+          if (rowIndex == groupsAndStudents.length &&
+              groupsAndStudents.isNotEmpty) {
+            return const TableCell.content(
+              colorHorizontalBorder: Colors.transparent,
+              colorVerticalBorder: Colors.transparent,
+            );
+          }
+          // the ungrouped title
+          if (rowIndex ==
+              groupsAndStudents.length +
+                  (groupsAndStudents.isNotEmpty ? 1 : 0)) {
+            return const TableCell.content();
+          }
+          // the ungrouped
+          return TableCell.content(
+            widget: Expanded(
+              child: Center(
+                child: AssessmentScore(
+                    score: assessments[columnIndex].scoreMap[ungroupedStudents[
+                            rowIndex -
+                                groupsAndStudents.length -
+                                1 -
+                                (groupsAndStudents.isNotEmpty ? 1 : 0)]] ??
+                        -1),
+              ),
+            ),
+          );
+        },
+        legendCell: const TableCell.legend(),
+      ),
+    );
+  }
+}
+
+class TableCell extends StatelessWidget {
+  const TableCell.content({
+    this.widget,
+    Key? key,
+    this.onTap,
+    this.colorHorizontalBorder,
+    this.colorVerticalBorder = Colors.black38,
+  }) : super(key: key);
+
+  const TableCell.legend({
+    this.widget,
+    Key? key,
+    this.onTap,
+    this.colorHorizontalBorder,
+    this.colorVerticalBorder = Colors.black38,
+  }) : super(key: key);
+
+  const TableCell.stickyRow({
+    this.widget,
+    Key? key,
+    this.onTap,
+    this.colorHorizontalBorder,
+    this.colorVerticalBorder = Colors.black38,
+  }) : super(key: key);
+
+  const TableCell.stickyColumn({
+    this.widget,
+    Key? key,
+    this.onTap,
+    this.colorVerticalBorder = Colors.black38,
+    this.colorHorizontalBorder,
+  }) : super(key: key);
+
+  final Function()? onTap;
+
+  final Widget? widget;
+
+  final Color? colorHorizontalBorder;
+  final Color? colorVerticalBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            left: colorHorizontalBorder == null
+                ? BorderSide.none
+                : BorderSide(color: colorHorizontalBorder!),
+            right: colorHorizontalBorder == null
+                ? BorderSide.none
+                : BorderSide(color: colorHorizontalBorder!),
           ),
-          for (Student student in students)
-            if (student.groups == null || student.groups!.isEmpty)
-              DataRow(
-                cells: <DataCell>[
-                  DataCell(Row(children: [
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Text(
-                      student.name,
-                      style: const TextStyle(fontStyle: FontStyle.italic),
-                    )
-                  ])),
-                  for (Assessment assessment in assessments)
-                    DataCell(Center(
-                      child: AssessmentScore(
-                          score: assessment.scoreMap[student] ?? -1),
-                    )),
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: [
+                  widget ?? Container(),
                 ],
               ),
-        ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 1.1,
+              color: colorVerticalBorder,
+            ),
+          ],
+        ),
       ),
     );
   }
